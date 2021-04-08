88f3809fcd2d849ffb13d4d1d1a66d11320aba40
shiva-12345 <munnoorushiva07@gmail.com>
2021-04-08 15:56:21 +0530

    single player mode

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to single player mode");
+        }
+    }
+}
