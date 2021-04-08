c09612b38e67603d8e5e502571913d111bec4aed
shiva-12345 <munnoorushiva07@gmail.com>
2021-04-08 16:01:17 +0530

    rolling dice

---
+++ SnakeLadderUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadder
+{
+    public class SnakeLadderUtility
+    {
+
+        public int diceRoll()
+        {
+            Random random = new Random();
+            int dice = random.Next(0, 7);
+            return dice;
+        }
+    }
+}
